1bd476ca9609d8e4d8046537c0290093d4abc7ea
anand498 <anand.pandey803@gmail.com>
2020-06-19 15:41:47 +0530

    changing the design for calculation

--- predict.py
+++ predict.py
@@ -1,14 +1,7 @@
-from keras.preprocessing.image import img_to_array
-from scipy.spatial import distance as dist
-from imutils.video import FileVideoStream
-from imutils.video import VideoStream
 import numpy as np
 import cv2,os,sys,dlib,imutils
-from PIL import Image
 from keras.models import load_model,Model
-from keras.models import Model,load_model
 from keras.preprocessing.image import img_to_array
-
 
 def mask(frame):
     sigma=0.5
@@ -18,26 +11,34 @@ def mask(frame):
     upper = int(min(255, (1.0 + sigma) * v))
     edged = cv2.Canny(gray_image, lower, upper)
     return edged
-def prediction_frame(croppedframe,size):
-    croppedframe=cv2.cvtColor(croppedframe,cv2.COLOR_GRAY2BGR)
-    croppedframe=cv2.resize(croppedframe,(size,size))
-    croppedframe = np.expand_dims(croppedframe, axis=0)
-    croppedframe.reshape([1,size,size,3]).astype('float32')/255.0
-    return croppedframe
+
+def prediction_frame(frame,size):
+    frame=cv2.cvtColor(frame,cv2.COLOR_GRAY2BGR)
+    frame=cv2.resize(frame,(size,size))
+    frame = np.expand_dims(frame, axis=0)
+    frame.reshape([1,size,size,3]).astype('float32')/255.0
+    return frame
+
 def values( a,  b):
+    a=int(a)
+    b=int(b)
     sum=a+b
-    diff= a-b
+    diff= a-b 
     product = a*b
     return sum,diff,product
+
 def predictframe():
-    FONT_HERSHEY_COMPLEX = 3
     video_capture = cv2.VideoCapture(0)
-    framerate = video_capture.get(cv2.CAP_PROP_FPS)
     framecount=0
-    flagforprediction=False
     size=128
-    left_result=0
-    right_result=0
+    flagforprediction=False
+    arr=[]
+    thickness=1
+    fontsize=0.6
+    (red,green,blue)=((0,0,255),(0,255,0),(255,0,0))
+    font=cv2.FONT_HERSHEY_COMPLEX
+    display = np.zeros((480, 480, 3), dtype=np.uint8)
+	
     while(True):
         keypressed = cv2.waitKey(1)
         if keypressed == ord('q'):
@@ -46,45 +47,42 @@ def predictframe():
             flagforprediction=True
         if keypressed == ord('x'):
             flagforprediction=False
-
+            cv2.destroyWindow('Cropped Frame')
         ret,frame = video_capture.read()
         frame1=frame
-        # frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        leftframe=frame[100:300,50:250]
-        rightframe=frame[100:300,400:600]
-        cv2.rectangle(frame, (50, 100),(250, 300), (255, 0, 0), 2)
-        cv2.rectangle(frame,(400,100),(600,300),(255,0,0),2)
+        frame2=frame
+        digitframe=frame[100:300,400:600]
+        operatorframe=frame[100:300,10:210]
+        cv2.putText(display,"Digit Window ", (400, 90),font,fontsize,green,thickness)
+        cv2.putText(frame1,"Operator Window ", (10, 90),font,fontsize,green,thickness)
+        cv2.rectangle(frame, (10, 100),(210, 300),blue, 1)
+        cv2.rectangle(frame,(400,100),(600,300),blue,1)
         if flagforprediction:
-            leftframe=mask(leftframe)
-            rightframe=mask(rightframe)
-            cv2.imshow('Left Frame',leftframe)
-            # cv2.resizeWindow('Left Frame', 256, 256)
-            cv2.imshow('Right Frame',rightframe)
-            # cv2.resizeWindow('Right Frame', 300, 300)
-            leftframe=prediction_frame(leftframe,size)
-            rightframe=prediction_frame(rightframe,size)
-            # if(framecount%(15)==0): # prediction after every 15 frames
-            leftdigit=np.argmax(model.predict(leftframe))
-            rightdigit=np.argmax(model.predict(rightframe))
-            left_result=labels.get(str(leftdigit))
-            right_result=labels.get(str(rightdigit))
-            print("left value"+str(left_result))
-            print("right value"+str(right_result))
-            cv2.putText(frame1,"Left value"+ str(left_result), (10, 70),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 1)
-            cv2.putText(frame1,"Right value"+ str(right_result), (10, 90),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 1)
-            # if(int(left_result)==0 || int(right_result)==0):
-
+            digitframe=mask(digitframe)
+            operatorframe=mask(operatorframe)
+            cv2.imshow('Operator',operatorframe)
+            cv2.imshow('Digit ',digitframe)
+            digitframe=prediction_frame(digitframe,size)
+            operatorframe=prediction_frame(operatorframe,size)
+            if(framecount%4==0):
+                digit=str(np.argmax(model.predict(digitframe)))
+                operator=str(np.argmax(operatormodel.predict(operatorframe)))
+                # operator=labels.get(str(operator))
+                arr.append(digit)
+            if(digit!='0'):
+                cv2.putText(display," Number"+ digit, (10, 40),font,fontsize,red,thickness)
+                cv2.putText(display," Operator"+ operator, (200, 40),font,fontsize,red,thickness)
+                
+            if(digit=='0'):
+                # flagforprediction=False
+                prevright=arr[len(arr)-2]
+                cv2.putText(display,"Previous Value "+ prevright, (120, 40),font,fontsize,green,thickness)
             framecount+=1
-        cv2.imshow('frame',frame1)
-        if keypressed == ord('d'):
-            flagforprediction=False
-            cv2.putText(frame1,"left Value = "+str(left_result), (60, 90),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-            cv2.putText(frame1,"Right Value = "+str(right_result), (60, 100),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-            cv2.putText(frame1,"Sum = "+str(int(right_result)+int(left_result)), (60, 110),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-            cv2.imshow('Total',frame1)
+            frame=np.hstack((frame,display))
+        cv2.imshow('frame',frame)
 if __name__ == '__main__':
-    value=0
     model=load_model('C:/Users/anand/Downloads/thirdtestmodel.h5')
-    labels={'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}
+    operatormodel=load_model('C:/Users/anand/Downloads/secondoperatormodel.h5')
+    labels={'0':'add','1':'cool','2':'palm','3':'yo','4':'next'}
     predictframe()
 	
