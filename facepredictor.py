11d6165b55091eedd7e33857b0c930fc5eb3b250
anand498 <anand.pandey803@gmail.com>
2020-06-21 14:42:30 +0530

    changing the img processing to thresholding+otsu

--- facepredictor.py
+++ facepredictor.py
@@ -16,35 +16,43 @@ def mask(frame):
 		upper = int(min(255, (1.0 + sigma) * v))
 		edged = cv2.Canny(gray_image, lower, upper)
 		return edged
-def captureframe():
+def binmask(frame):
+		gray = frame[:, :, 2]
+		ret, thresh_gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+		thresh_gray = cv2.morphologyEx(thresh_gray, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3)))
+		thresh_gray = cv2.morphologyEx(thresh_gray, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7,7)))
+		return thresh_gray
+def captureframe(value):
 	video_capture = cv2.VideoCapture(0)
-	framerate = video_capture.get(cv2.CAP_PROP_FPS)
-	print(framerate)
 	framecount = 0
 	flagforprediction = False
 	digitcount=0
+	framenum=0
 	while(True):
 		keypress= cv2.waitKey(1)
+		ret,frame = video_capture.read()
 		if  keypress == ord('q'):
-					break 
+    			break
 		if  keypress ==  ord('c'):
     			flagforprediction = True		
-		ret,frame = video_capture.read()
-		frame1=frame
 		framecount+=1
-		totalframes=200
-		cv2.imshow('Masked',mask(frame1))
-		cv2.rectangle(frame, (23, 113),(237, 352), (255, 0, 0), 1)
+		totalframes=500
+		cv2.rectangle(frame, (21, 111),(175, 265), (255, 0, 0), 1)
 		if flagforprediction==True:
-				if(framecount%3==0 and digitcount!=totalframes):
+				if(digitcount!=totalframes and framecount%2==0):
 						print('Captured : '+str(digitcount))
 						digitcount+=1
 						cv2.putText(frame,'Capturing', (300, 100),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-						# cv2.imwrite('C:/Users/anand/Desktop/Data/9-%d.png'%np.random.randint(1,40000),mask(frame[115:350,25:235]))
+						cv2.imwrite('C:/Users/anand/Desktop/masks/%d-%d.png'%(int(value),framenum),binmask(frame[113:263,23:173]))
+						framenum+=1
 						if (digitcount==totalframes):
 							flagforprediction= False
+							break
 		cv2.imshow('Actual Frame',frame)
-		cv2.imshow('Masked frame',mask(frame))		
+		cv2.imshow('Mask Frame',binmask(frame))
+
 if __name__ == '__main__':
-	captureframe()
+	print('Enter the value')
+	a=input()
+	captureframe(a)
 	
