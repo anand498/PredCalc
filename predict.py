33bdcd000a9eda663e048ed4b7bfa6fd4600da40
anand498 <anand.pandey803@gmail.com>
2020-06-23 14:22:09 +0530

    replacing calc function with eval

--- predict.py
+++ predict.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-from keras.models import load_model,Model
+from keras.models import load_model
  
 
 def prediction_frame(model,frame):
@@ -16,31 +16,14 @@ def binmask(frame):
         thresh_gray = cv2.morphologyEx(thresh_gray, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3)))
         thresh_gray = cv2.morphologyEx(thresh_gray, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7,7)))
         return thresh_gray
-def calc(a):
-    op=['+','-','*','/']
-    operand=''
-    for i in range(len(a)):
-        if(a[i] in op ):
-            value1=int(a[0:i])
-            value2=int(a[i+1:len(a)-1])
-            operand=a[i]
-         
-        elif (a[i]=='='):
-            if(operand=='+'):
-                result = value1+value2
-            elif (operand=='-'):
-                result=value1-value2
-            elif (operand=='*'):
-                result=value1*value2        
-    return str(result)
 def hand_calc():
     framecount=0
     dig_pred=False
-    text=""
-    oldtext=""
-    digit=""
+    text,oldtext,digit=('','','')
+    # text=""
+    # oldtext=""
+    # digit=""
     sametext=0
-    equal=True
     totalframecheck=40
     while(True):
         display = np.zeros((480, 600, 3), dtype=np.uint8)
@@ -54,28 +37,28 @@ def hand_calc():
         if keypressed == ord('o'):
             cv2.destroyWindow('Cropped Frame')
         ret,frame = video_capture.read()
-        digitframe=frame[113:263,23:173]
+        digitframe=frame[113:263,23:173] #cropped frame
         digitframe=binmask(digitframe)
-        cv2.putText(frame,"Digit Window",(10, 90),font,fontsize,red,thickness)
+        cv2.putText(frame,"Digit Window",(10, 90),font,fontsize,red,thickness) #display the text digit window
         cv2.imshow('Digit Mask ',digitframe)
-        cv2.rectangle(frame, (21, 111),(175, 265),blue, 1)
+        cv2.rectangle(frame, (21, 111),(175, 265),blue, 1) #create a rectangle for the region of interet segmentation
         if dig_pred:
             oldtext=digit
-            digit=prediction_frame(maskmodel,digitframe)
-            if oldtext==digit:
+            digit=prediction_frame(maskmodel,digitframe) # predict the segmented ROI
+            if oldtext==digit: #check if values are the same
                 sametext+=1 
             else:
                 sametext=0
-            if sametext==totalframecheck and digit=='0':
+            if sametext==totalframecheck and digit=='0': # to clear the screen
                 text=''
                 sametext=0
-            if sametext==totalframecheck:
-                if(digit!='13'):
+            if sametext==totalframecheck:# if the value is detected for a ceratin number of consecutive frames
+                if(digit!='13'):# to check for the digit it should not be the '=' sign
                     text+=labels.get(digit)
                     sametext=0
                 if digit=='13':
-                    text+='='
-                    text+=calc(text)
+                    print(text[:len(text)-1])
+                    text+=str(eval(text[:len(text)-1]))
                     sametext=0
         
         framecount+=1
@@ -86,10 +69,10 @@ def hand_calc():
         
 if __name__ == '__main__':
     (red,green,blue)=((0,0,255),(0,255,0),(255,0,0))
-    font=cv2.FONT_HERSHEY_TRIPLEX
-    thickness=1
-    fontsize=0.6
-    text=""
+    font,thickness,fontsize=(cv2.FONT_HERSHEY_TRIPLEX,1,0.6)
+    # font=cv2.FONT_HERSHEY_TRIPLEX
+    # thickness=1
+    # fontsize=0.6
     maskmodel=load_model('C:/Users/anand/Downloads/maskmodelv1.h5')
     # '0':'','1':'1','2':'2','3':'3','4':'4','5':'5','6':'6','7':'7','8':'8','9':'9','10':'add','11':'sub','12':'multiply','13':'divide'
     labels={'0':'Empty','1':'1','2':'2','3':'3','4':'4','5':'5','6':'6','7':'7','8':'8','9':'9','10':'+','11':'-','12':'*','13':'='}
